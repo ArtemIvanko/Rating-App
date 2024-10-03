814cc8bd5b8d5c1611cb446b198bee593572c425
ArtemIvanko <tema30k@gmail.com>
2024-10-03 19:48:46 +0300

    Used navigation route, added index route

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,10 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigation } from "@shared/Navigation";
 
 export const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<div>Home page</div>} />
-      <Route path="*" element={<div>Not Found</div>} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Navigate to="/home" />} />
+        <Route path="home" element={<div>123</div>} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
