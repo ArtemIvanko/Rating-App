5cf00c2bdf6e16ed49514109fddcce7bd8ee5744
ArtemIvanko <tema30k@gmail.com>
2024-10-03 19:24:15 +0300

    Created and used theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
 import { Router } from "./pages/Router";
+import { Providers } from "./Providers";
 
-export const App = () => <Router />;
+export const App = () => (
+  <Providers>
+    <Router />
+  </Providers>
+);
