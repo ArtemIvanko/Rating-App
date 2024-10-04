1ba9f8d7b89dc5c3f9b8d0812f71ef7b73afcc3f
ArtemIvanko <tema30k@gmail.com>
2024-10-04 11:32:50 +0300

    Used custom form in login page

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,41 +1,18 @@
-import styled from "@/DefaultTheme";
-import { Button, TextField, Typography } from "@mui/material";
+import { Form } from "@utils/Form";
+import { TextField } from "@mui/material";
 
 export const Login = () => {
   return (
-    <Form>
-      <Typography variant="h5">Welcome back!</Typography>
-      <Typography variant="body2">
-        Please enter your details to sign in
-      </Typography>
-      <FieldsContainer>
+    <Form
+      title="Welcome back!"
+      subtitle="Please enter your details to sign in"
+      buttonLabel="Sign in"
+      children={
         <TextField
-          label="E-Mail or Username"
+          label="E-mail or Username"
           placeholder="Enter your email/username"
         />
-      </FieldsContainer>
-      <Button type="submit" variant="contained" fullWidth>
-        Sign in
-      </Button>
-    </Form>
+      }
+    />
   );
 };
-
-const Form = styled("form")({
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  width: "400px",
-  height: "600px",
-  background: "white",
-  padding: "1rem 2.5rem",
-});
-
-const FieldsContainer = styled("div")({
-  display: "flex",
-  flexDirection: "column",
-  gap: "1rem",
-  width: "100%",
-  margin: "1.5rem 0",
-});
