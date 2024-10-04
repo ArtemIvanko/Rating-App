38cf32870fa47e32d791edb2155b8069f48ae7ce
ArtemIvanko <tema30k@gmail.com>
2024-10-04 14:06:05 +0300

    Created and used Dialog

---
+++ src/components/utils/Dialog/Dialog.tsx
@@ -0,0 +1,32 @@
+import { FC, memo } from "react";
+import {
+  Dialog as MUIDialog,
+  DialogProps as MUIDialogProps,
+} from "@mui/material";
+
+import styled from "@/DefaultTheme";
+
+export interface IDialogProps extends MUIDialogProps {
+  onClose?: () => void;
+  isActive?: boolean;
+}
+
+export const Dialog: FC<IDialogProps> = memo(
+  ({ onClose, fullWidth = true, children, isActive, ...restDialogProps }) => {
+    return (
+      <StyledDialog
+        {...restDialogProps}
+        fullWidth={fullWidth}
+        onClose={onClose}
+      >
+        {children}
+      </StyledDialog>
+    );
+  },
+);
+
+const StyledDialog = styled(MUIDialog)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
