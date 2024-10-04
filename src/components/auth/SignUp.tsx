26b58ed220550e055d1ff455ad559113af17ce2a
ArtemIvanko <tema30k@gmail.com>
2024-10-04 13:29:54 +0300

    Created and used SignUp form

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,3 +1,22 @@
+import { Form, FormTextField, PasswordFieldText } from "@utils/Form";
+import { useSignUp } from "@/hooks";
+
 export const SignUp = () => {
-  return <div>SignUp</div>;
+  const { control, handleSubmit } = useSignUp();
+
+  return (
+    <Form
+      title="Welcome back!"
+      subtitle="Please enter your details to sign in"
+      buttonLabel="Sign in"
+      children={
+        <>
+          <FormTextField name="username" control={control} />
+          <FormTextField name="email" control={control} />
+          <PasswordFieldText name="password" control={control} />
+        </>
+      }
+      onSubmit={handleSubmit}
+    />
+  );
 };
