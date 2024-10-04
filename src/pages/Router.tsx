e08da9d4632b7c570e078fcc1a201f59759b9dc8
ArtemIvanko <tema30k@gmail.com>
2024-10-04 14:03:38 +0300

    Created and used Outlet. Removed unnecessary routes

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Navigation } from "@shared/Navigation";
-import { Login, SignUp } from "@shared/auth";
+import { Outlet } from "@shared/Navigation";
 
 export const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path="/" element={<Outlet />}>
         <Route index element={<Navigate to="/home" />} />
         <Route path="home" element={<div>123</div>} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
         <Route path="*" element={<div>Not Found</div>} />
       </Route>
     </Routes>
