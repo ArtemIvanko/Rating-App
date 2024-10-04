7379ce2afd6963c0d5b8550169c8c7e9fd4e867b
ArtemIvanko <tema30k@gmail.com>
2024-10-04 18:52:45 +0300

    Updated firebaseConfig due to registration

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,3 +24,6 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 export default app;
+
+export const database = getDatabase(app);
+export const auth = getAuth(app);
