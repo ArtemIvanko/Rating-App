bd5f106b06bbc27e88f0ae7a4d69ae9350cbb952
ArtemIvanko <tema30k@gmail.com>
2024-10-04 11:32:13 +0300

    Created and used reusable auth form

---
+++ src/components/utils/Form/Form.tsx
@@ -0,0 +1,50 @@
+import { ReactNode } from "react";
+import styled from "@/DefaultTheme";
+import { Button, TextFieldProps, Typography } from "@mui/material";
+
+type IFormProps = TextFieldProps & {
+  children?: ReactNode;
+  onSubmit?: () => void;
+  title?: string;
+  subtitle?: string;
+  buttonLabel?: string;
+};
+
+export const Form = ({
+  children,
+  onSubmit,
+  title,
+  subtitle,
+  buttonLabel,
+  ...restProps
+}: IFormProps) => {
+  return (
+    <StyledForm onSubmit={onSubmit}>
+      <Typography variant="h5">{title}</Typography>
+      <Typography variant="body2">{subtitle}</Typography>
+      <FieldsContainer>{children}</FieldsContainer>
+      <Button type="submit" fullWidth title={buttonLabel}>
+        {buttonLabel}
+      </Button>
+    </StyledForm>
+  );
+};
+
+const StyledForm = styled("form")({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "400px",
+  height: "600px",
+  background: "white",
+  padding: "1rem 2.5rem",
+});
+
+const FieldsContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+  width: "100%",
+  margin: "1.5rem 0",
+});
