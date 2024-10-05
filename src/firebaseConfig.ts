f827b366f563563878823f3c0b32826b4f1626e2
ArtemIvanko <tema30k@gmail.com>
2024-10-05 17:11:02 +0300

    Used real data instead of .env

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -9,14 +9,15 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyA-dCtJfTBx2VxnplNq9ZrOepO1OsbliHM",
+  authDomain: "rating-app-45486.firebaseapp.com",
+  databaseURL:
+    "https://rating-app-45486-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "rating-app-45486",
+  storageBucket: "rating-app-45486.appspot.com",
+  messagingSenderId: "1046744546210",
+  appId: "1:1046744546210:web:540fe4f4b060c5e4da50fb",
+  measurementId: "G-H6XRWRBRHZ",
 };
 
 // Initialize Firebase
