1938db99851cd9c88c5332948f6e18b4945a342a
ArtemIvanko <tema30k@gmail.com>
2024-10-05 16:30:26 +0300

    Used global imports for comment components

--- src/components/Feedback/index.ts
+++ src/components/Feedback/index.ts
@@ -1,3 +1,5 @@
 export * from "./Rating/AverageRating";
 export * from "./Rating/RatingComponent";
+export * from "./Comments/CommentsList";
+export * from "./Comments/CommentComponent";
 export * from "./ItemDetail";
